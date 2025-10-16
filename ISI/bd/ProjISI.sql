e03f486d582f11c0b0221104f5fbc0e15710c4ea
Hugo Monteiro <a27993@alunos.ipca.pt>
2025-10-16 19:30:13 +0100

    html e sendemail

--- ISI/bd/ProjISI.sql
+++ ISI/bd/ProjISI.sql
@@ -87,20 +87,20 @@ Nome varchar(255) NOT NULL,
 Email varchar(255) NOT NULL,
 Contacto varchar(255) NOT NULL,
 Pais varchar(255) NOT NULL,
-Categoria varchar(255) NOT NULL,
 Ativo bit NOT NULL DEFAULT 1
 );
 
-INSERT INTO Utilizadores(Nome, Email, Contacto, Pais, Categoria, Ativo)
+INSERT INTO Utilizadores(Nome, Email, Contacto, Pais, Ativo)
 VALUES
-('Ana Ferreira', 'ana.ferreira@gmail.com', '912345678', 'Portugal', 'red', 0),
-('João Martins', 'joao.martins@hotmail.com', '934567890', 'France', 'white', 0),
-('Maria Oliveira', 'maria.oliveira@gmail.com', '965432198', 'Italy', 'red', 0),
-('Pedro Sousa', 'pedro.sousa@email.com', '918765432', 'Spain', 'sparkling', 0),
-('Inês Rocha', 'ines.rocha@email.com', '937654321', 'United States', 'white', 0),
-('Ricardo Gomes', 'ricardo.gomes@gmail.com', '915678234', 'Argentina', 'red', 0),
-('Carla Ribeiro', 'carla.ribeiro@hotmail.com', '963482716', 'Portugal', 'sparkling', 0),
-('Tiago Monteiro', 'tiago.monteiro@gmail.com', '928374651', 'France', 'red', 0),
-('Sofia Nunes', 'sofia.nunes@email.com', '914726391', 'Italy', 'white', 0),
-('Miguel Costa', 'miguel.costa@hotmail.com', '931847265', 'Spain', 'rose', 0),
-('Hugo Monteiro', 'projisi@hotmail.com', '123456789', 'Italy', 'white', 1);
+('Ana Ferreira', 'ana.ferreira@gmail.com', '912345678', 'Portugal', 0),
+('João Martins', 'joao.martins@hotmail.com', '934567890', 'France', 0),
+('Maria Oliveira', 'maria.oliveira@gmail.com', '965432198', 'Italy', 0),
+('Pedro Sousa', 'pedro.sousa@email.com', '918765432', 'Spain', 0),
+('Inês Rocha', 'ines.rocha@email.com', '937654321', 'United States', 0),
+('Ricardo Gomes', 'ricardo.gomes@gmail.com', '915678234', 'Argentina', 0),
+('Carla Ribeiro', 'carla.ribeiro@hotmail.com', '963482716', 'Portugal', 0),
+('Tiago Monteiro', 'tiago.monteiro@gmail.com', '928374651', 'France', 0),
+('Sofia Nunes', 'sofia.nunes@email.com', '914726391', 'Italy', 0),
+('Miguel Costa', 'miguel.costa@hotmail.com', '931847265', 'Spain', 0),
+('Hugo Monteiro', 'projisi@hotmail.com', '123456789', 'Italy', 1);
+
